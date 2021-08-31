954ba340a9da8eca95a0f61aebb0f3f5b0daea4d
Timothy Quartey <kofitimmie@hotmail.com>
2021-08-31 15:11:58 -0400

    Update to app.R
    Provided 4 graphs for the user to interact with based on specific filters, added a datatable underneath the graphs. On the table tab the user can also filter a datatable based on the data, as well as a link to the code, currently in Github

--- app.R
+++ app.R
@@ -1,7 +1,15 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(readr)
+library(magrittr)
+library(dplyr)
+library(DT)
 
+mtcars2 <- read_csv("mtcars2.csv")
+colors <- c("#EE4B2B","#FF6E33","#F9FF33","#A3FF33",
+            "#65FF33","#94FF33","#50FF33", "#3388FF",
+            "#335DFF", "#3385FF", "#F9FF33", "#FFA833")
 ui <- dashboardPage(
   dashboardHeader(title = "My Dashboard", titleWidth = 300,
                   dropdownMenu(type = "messages",
@@ -59,27 +67,91 @@ ui <- dashboardPage(
     sidebarSearchForm(textId = "searchText", buttonId = "searchButton",label = "Search..."),
     menuItem("Graphs", tabName = 'graphs', icon = icon("chart-bar")),
     menuItem("Table", tabName = 'table', icon = icon("signal")),
-    menuItem("Source Code for app", tabName = 'code', icon = icon("pencil"))
+    menuItem("Source code for app", icon = icon("pencil"), 
+             href = "https://github.com/tquart1/Graphs_and_Plots/blob/main/app.R")
   )
   ),
+  #took away main panels to decrease graph whitespace
   dashboardBody(
     tabItems(
     tabItem("graphs",
             fluidRow(
-              box(plotOutput("lineg")),
-              box(sidebarLayout(
-                selectInput("carDisplay", label = h5("Favorite category"),
-                            choices = c("mpg", "disp", "hp", "wt", "qsec"), selected = "mpg"),
-                selectInput("carOther", label = h5("Secondary category"),
-                            choices = c("cyl", "drat", "vs"), selected = "cyl")
-              ),
-                mainPanel(plotOutput("barg"))
+              box(
+                selectInput(inputId = "carType", label = "Choose a CarType",
+                           "carType"),
+                plotOutput("lineg")
                 ),
-              box(plotOutput("donutg"))
+              box(
+                selectInput(inputId = "carSpeed",
+                            label = "Choose a Speed",
+                           "carSpeed"),
+                #sliderInput("disp", "Disp",min = 100, max = 400, value =100),
+                plotOutput("barg")
+                ),
+              box(
+                radioButtons("plot_type", "Plot type",
+                             c("base", "ggplot2")),
+                plotOutput("plot1")
+              ),
+              box(
+                selectInput("carGo", "Speed:", choices = unique(mtcars2$carSpeed)),
+                selectInput("carCountry", "Country: ", choices = unique(mtcars2$original)),
+                #selectInput("carAm", "Am: ", choices = unique(mtcars2$am)),
+                
+                  plotOutput("car")
+                
+              ),
+              
+              fluidPage(
+                h3("Mtcars2 data"),
+                fluidRow(
+                  column(4,
+                         selectInput("mpg",
+                                     "Mpg:",
+                                     c("All",
+                                       unique(as.character(mtcars2$mpg))))
+                  ),
+                  column(4,
+                         selectInput("hp",
+                                     "Horsepower:",
+                                     c("All",
+                                       unique(as.character(mtcars2$hp))))
+                  ),
+                  column(4,
+                         selectInput("cyl",
+                                     "Cylinders:",
+                                     c("All",
+                                       unique(as.character(mtcars2$cyl))))
+                  )
+                ),
+                dataTableOutput("mtcars2display")
+              )
+              
+              #box(plotOutput("donutg"))
             )),
     tabItem("table",
             fluidPage(
               h3("The cars dataset"),
+              fluidRow(
+                column(4,
+                       selectInput("mpg",
+                                   "Mpg:",
+                                   c("All",
+                                     unique(as.character(mtcars2$mpg))))
+                ),
+                column(4,
+                       selectInput("hp",
+                                   "Horsepower:",
+                                   c("All",
+                                     unique(as.character(mtcars2$hp))))
+                ),
+                column(4,
+                       selectInput("cyl",
+                                   "Cylinders:",
+                                   c("All",
+                                     unique(as.character(mtcars2$cyl))))
+                )
+              ),
               dataTableOutput("mtcarsdisplay")
             )),
     tabItem("code")
@@ -87,22 +159,124 @@ ui <- dashboardPage(
   )
 )
 
-server <- function(input, output) { 
-  output$lineg <- renderPlot({
-    ggplot(mtcars) + geom_line(aes(x = hp, y = mpg, color = factor(gear))) + ggtitle("Lineplot Mtcars")
+server <- function(input, output, session) { 
+  #Line chart -- Interactive - 1 param
+ linedat <- reactive({
+   req(input$carType)
+   df <- mtcars2 %>% filter(carType == input$carType) 
+ })
+  observe({
+     updateSelectInput(session, "carType", choices = unique(mtcars2$carType))
+   })
+
+   output$lineg <- renderPlot({
+      ggplot(linedat(), aes(x = X1, y = wt, color = wt, group = 1)) + geom_line()
+   })
+  #Bar Chart -- Interactive - 1 param
+  observe({
+    updateSelectInput(session, "carSpeed", choices = mtcars2$carSpeed)
   })
-  output$barg <- renderPlot({
-    ggplot(mtcars, aes_string(x = input$carDisplay)) +
-      geom_bar(aes_string(fill= input$carOther), position = position_stack(reverse = TRUE)) +
-      coord_flip() + 
-      theme(legend.position = "top")
-  })
-  output$donutg <- renderPlot({
-   ggplot(mtcars2, aes(ymax = mtcars2$ymax, ymin = mtcars2$ymin, xmax = 4, xmin=3, fill=category)) + geom_rect()+
-     coord_polar(theta = "y") + xlim(c(2,4))
+  data <- reactive({
+   req(input$carSpeed)
+    df <- mtcars2 %>% filter(carSpeed %in% input$carSpeed) %>%  group_by(X1) %>% summarize(dispMedian = median(disp))
   })
   
-  output$mtcarsdisplay <- renderDataTable(mtcars)
-  }
+  output$barg <- renderPlot({
+   ggplot(data(), aes(x = X1, y = dispMedian)) + geom_bar(stat = "identity", fill = "steelblue")  
+    })
+  
+  #Donut Chart -- non-Interactive
+  tidonut <- reactive({
+    donut <- mtcars2 %>% group_by(X1) %>% summarize(counts = n(), percentage = n()/nrow(mtcars2))
+  })
+ 
+  output$donutg <- renderPlot({
+    # donut <- mtcars2 %>%
+    #   group_by(X1) %>%
+    #   summarize(counts = n(), percentage = n()/nrow(mtcars2))
+    
+   ggplot(tidonut(), aes(x = 2, y = percentage, fill = X1)) + geom_col(color = "black")+
+     coord_polar("y", start = 0) + geom_text(aes(label = paste0(round(percentage*100),"%")),
+                                             position = position_stack(vjust = 0.5)) +
+     theme(panel.background = element_blank(),
+           axis.line = element_blank(),
+           axis.text = element_blank(),
+           axis.ticks = element_blank(),
+           axis.title = element_blank(), 
+           plot.title = element_text(hjust = 0.5, size = 18)) +
+     ggtitle("Donut Chart of Subset Mtcars(ggplot2)") +
+     scale_fill_manual(values = colors) +
+     xlim(0.5, 2.5)
+  })
+  
+  #Bar Plot -- Interactive, 2 params
+ thedata <- reactive({
+   req(input$carSpeed)
+   #req(input$original)
+   
+   df <- mtcars2 %>% filter(carSpeed == input$carGo) %>% filter(original == input$carCountry)
+ })
+  observe({
+    updateSelectInput(session, "carSpeed", choices = unique(mtcars2$carSpeed))
+  })
+ #  observe({
+ #    updateSelectInput(session, "carWt", choices = unique(mtcars2$carWt))
+ # })
+output$car <- renderPlot({
+  
+  ggplot(thedata(), aes(x = X1, y = mpg, color = "carType", group = 4)) + geom_line(size = 1)
+  
+   })
 
+# mtcars2 %>% filter( carSpeed == 'M') %>% filter(original == 'America') %>%
+# ggplot(mtcars2, mapping = aes(x = X1, y = mpg, color = "carType", group = 2)) + geom_line()
+
+#Check Box bar -- Interactive 1 param
+  checkdata <- reactive({
+    mtcars2
+  })
+  observe({
+    choices <- c("base", "ggplot2")
+    updateCheckboxInput(session, "plot_type", value = choices)
+  })
+  
+  output$plot1 <- renderPlot({
+    if (input$plot_type == "base") {
+      plot(mtcars2$wt, mtcars2$mpg)
+    } else if (input$plot_type == "ggplot2") {
+      ggplot(checkdata(), aes(wt, mpg)) + geom_point()
+    }
+  })
+  
+  output$mtcars2display <- DT::renderDataTable(DT::datatable({
+    data <- mtcars2
+    if (input$mpg != "All") {
+      data <- data[data$mpg == input$mpg,]
+    }
+    if (input$hp != "All") {
+      data <- data[data$hp == input$hp,]
+    }
+    if (input$cyl != "All") {
+      data <- data[data$cyl == input$cyl,]
+    }
+    data
+  }))
+  
+  
+  #Table Page
+  output$mtcarsdisplay <- DT::renderDataTable(DT::datatable({
+    data <- mtcars
+    if (input$mpg != "All") {
+      data <- data[data$mpg == input$mpg,]
+    }
+    if (input$hp != "All") {
+      data <- data[data$hp == input$hp,]
+    }
+    if (input$cyl != "All") {
+      data <- data[data$cyl == input$cyl,]
+    }
+    data
+  }))
+  
+}
 shinyApp(ui, server)
